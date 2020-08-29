cd8d1ba6d00f3c62f1044a6d509f662bd288136e
vallko <61822548+vallko@users.noreply.github.com>
2020-08-29 09:28:17 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,21 +9,22 @@ var table = document.getElementById("table1").getElementsByTagName('tbody')[0]
 var newRow = table1.insertRow()
 newRow.appendChild(inputTask);
 newRow.appendChild(inputPriority)
+let taskCounter = 1
 
 let taskDB = [];
-let taskCounter = 1
 var save1 = () => {
-    for(let i = 0; i < taskCounter; i++){
-        var newRow = table1.insertRow()
+    var newRow = table1.insertRow()
+    for( let i = 0; i < taskCounter; i++){
         readTask = inputTask.value
         readPriority = inputPriority.value
         taskManager = { 
             task: readTask , 
             priority: readPriority}
         taskDB.push(taskManager);
-        newRow.innerHTML = `${taskManager.task}  ${taskManager.priority}`
+        newRow.innerHTML = `${taskCounter} ${taskManager.task}  ${taskManager.priority}`
         localStorage["taskDB"] = JSON.stringify(taskDB)
         taskDB.push(JSON.parse(localStorage.getItem('session')));
       }
-      console.log(gete)
+      taskCounter++;
+
 }
