eaa2982fd47a02a3d6bb916a59f29d7401ee51e1
vallko <61822548+vallko@users.noreply.github.com>
2020-09-09 13:20:26 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const inputTask = document.createElement('input')
 inputTask.id = "taskField"
 inputTask.setAttribute('type', 'TEXT')
@@ -5,28 +6,36 @@ const inputPriority = document.createElement("INPUT")
 inputPriority.setAttribute('type', 'TEXT')
 inputTask.id = "priorityField"
 
-var table = document.getElementById("table1").getElementsByTagName('tbody')[0]
+var table = document.getElementById("table1").getElementsByTagName('tbody')
 var newRow = table1.insertRow()
-newRow.appendChild(inputTask);
+newRow.appendChild(inputTask)
 newRow.appendChild(inputPriority)
-let taskDB = [];
-let taskCounter = 2                                 
-let i = 1;                                                             
+let taskDB = []
+let mappedArray = []
+let taskCounter = 1                                
+let i = 1     
 
 var save1 = () => {
     var newRow = table1.insertRow()
-    for(;i < taskCounter; i++){
+    for(;i <= taskCounter; i++){
         readTask = inputTask.value
         readPriority = inputPriority.value
         taskManager = { 
-            task: readTask , 
-            priority: readPriority}
-        taskDB.push(taskManager);
-        newRow.innerHTML = `${i} ${taskManager.task}  ${taskManager.priority} `
+            task: readTask, 
+            priority: readPriority
+        }
+        taskDB.push(taskManager)
+        newRow.innerHTML = `${i} ${taskManager.task} ${taskManager.priority}` 
         localStorage["taskDB"] = JSON.stringify(taskDB)
-        taskDB.push(JSON.parse(localStorage.getItem('session')));
-      }
-      taskCounter++
-      
-console.log(JSON.stringify(taskDB))
-}
+        localStorage["mappedArray"] = JSON.stringify(mappedArray)
+        mappedArray = taskDB.map(individualTask => { const mappedTasks = {} 
+        mappedTasks.task = individualTask.task
+        mappedTasks.priority = individualTask.priority
+        return mappedTasks;
+        })
+        }
+        taskCounter++
+        console.log(JSON.stringify(mappedArray))
+      inputTask.value= ''
+      inputPriority.value = ''
+    }
